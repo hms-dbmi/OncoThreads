ce3d43568d0df0095fa63dbffec05e6a6c04bcf1
ThHarbig <theresa_ha@hotmail.de>
2018-07-12 16:14:50 -0400

    changed representation of continuous variable in grouped mode

--- src/modules/TemporalHeatmap/components/Timepoints/Group/CategoricalRow.jsx
+++ src/modules/TemporalHeatmap/components/Timepoints/Group/CategoricalRow.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import {observer} from 'mobx-react';
+import uuidv4 from "uuid/v4";
 /*
 creates a row in a partition of a grouped timepoint
  */
-const PartitionRow = observer(class PartitionRow extends React.Component {
+const CategoricalRow = observer(class CategoricalRow extends React.Component {
     static getTooltipContent(value, numPatients) {
         {
             let content = "";
@@ -30,15 +31,8 @@ const PartitionRow = observer(class PartitionRow extends React.Component {
                 }
                 fill = "white"
             }
-            let tooltipContent;
-            if(_self.props.variableType!=='NUMBER'){
-                tooltipContent=PartitionRow.getTooltipContent(f.key, f.value);
-            }
-            else{
-                tooltipContent=f.key;
-            }
             rects.push(<rect key={f.key}
-                             onMouseEnter={(e) => _self.props.showTooltip(e,tooltipContent)}
+                             onMouseEnter={(e) => _self.props.showTooltip(e, CategoricalRow.getTooltipContent(f.key, f.value))}
                              onMouseLeave={_self.props.hideTooltip} width={_self.props.groupScale(f.value)}
                              x={_self.props.groupScale(currCounts)} height={_self.props.height}
                              fill={fill} stroke={stroke} opacity={_self.props.opacity}/>);
@@ -48,9 +42,9 @@ const PartitionRow = observer(class PartitionRow extends React.Component {
     }
 
     render() {
-        return (
-            this.createRow()
-        )
+            return (
+                this.createRow()
+            )
     }
 });
-export default PartitionRow;
+export default CategoricalRow;
