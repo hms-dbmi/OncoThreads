315dd2a928d0439b9b0758dc513bbd6071c70ecc
ThHarbig <theresa_ha@hotmail.de>
2019-05-30 13:55:30 -0400

    Implemented random slanted lines and horizontal stacking

--- src/modules/TemporalHeatmap/components/Timepoints/Group/CategoricalRow.jsx
+++ src/modules/TemporalHeatmap/components/Timepoints/Group/CategoricalRow.jsx
@@ -5,6 +5,11 @@ import {inject, observer} from 'mobx-react';
  * Component representing a row of a categorical variable in a partition of a grouped timepoint
  */
 const CategoricalRow = inject("dataStore", "uiStore", "visStore")(observer(class CategoricalRow extends React.Component {
+    constructor() {
+        super();
+        this.previousOffset = 0;
+    }
+
     /**
      * Creates a tooltip showing information about the row
      * @param {string} value - category
@@ -30,63 +35,91 @@ const CategoricalRow = inject("dataStore", "uiStore", "visStore")(observer(class
         let rects = [];
         let currCounts = 0;
         let offset;
-        if (this.props.uiStore.slantedLines === "singleDir") {
-            offset = 3;
-        }
-        else if (this.props.uiStore.slantedLines === "altAcross") {
-            if (this.props.isEven) {
-                offset = 3;
+        if (!this.props.uiStore.horizontalStacking) {
+            if (this.props.uiStore.slantedLines === "singleDir") {
+                offset = this.props.height / 2;
             }
-            else {
-                offset = -3;
+            else if (this.props.uiStore.slantedLines === "altAcross") {
+                if (this.props.isEven) {
+                    offset = this.props.height / 2;
+                }
+                else {
+                    offset = -this.props.height / 2;
+                }
             }
-        }
-        else if (this.props.uiStore.slantedLines === "none") {
-            offset = 0;
-        }
-        let partitionPoints, selectedPoints;
+            else if (this.props.uiStore.slantedLines === "none") {
+                offset = 0;
+            }
 
-        this.props.row.forEach((f, i) => {
-            let fill = this.props.color(f.key);
-            let stroke = this.props.stroke;
-            if (f.key === undefined) {
-                if (stroke === "none") {
-                    stroke = "lightgray";
+            this.props.row.forEach((f, i) => {
+                let partitionPoints, selectedPoints;
+                let fill = this.props.color(f.key);
+                let stroke = this.props.stroke;
+                if (f.key === undefined) {
+                    if (stroke === "none") {
+                        stroke = "lightgray";
+                    }
+                    fill = "white"
                 }
-                fill = "white"
-            }
-            if (this.props.uiStore.slantedLines === "altWithin") {
-                if(i%2===0){
-                    offset=3;
+                if (this.props.uiStore.slantedLines === "altWithin") {
+                    if (i % 2 === 0) {
+                        offset = this.props.height / 2;
+                    }
+                    else {
+                        offset = -this.props.height / 2
+                    }
+                    partitionPoints = this.transformToPolygonAlternating(this.props.visStore.groupScale(currCounts), this.props.visStore.groupScale(f.patients.length), i, this.props.row.length - 1, offset);
+                    selectedPoints = this.transformToPolygonAlternating(this.props.visStore.groupScale(currCounts), this.props.visStore.groupScale(this.getSelected(f.patients)), i, this.props.row.length - 1, offset);
                 }
-                else{
-                    offset=-3;
+                else if (this.props.uiStore.slantedLines === "random") {
+                    offset = Math.random() * this.props.height - this.props.height / 2;
+                    partitionPoints = this.transformToPolygonRandom(this.props.visStore.groupScale(currCounts), this.props.visStore.groupScale(f.patients.length), i, this.props.row.length - 1, this.previousOffset, offset);
+                    selectedPoints = this.transformToPolygonRandom(this.props.visStore.groupScale(currCounts), this.props.visStore.groupScale(this.getSelected(f.patients)), i, this.props.row.length - 1, this.previousOffset, offset);
+                    this.previousOffset = offset
                 }
-                partitionPoints = this.transformToPolygonAlternating(this.props.visStore.groupScale(currCounts), this.props.visStore.groupScale(f.patients.length), i, this.props.row.length - 1, offset);
-                selectedPoints = this.transformToPolygonAlternating(this.props.visStore.groupScale(currCounts), this.props.visStore.groupScale(this.getSelected(f.patients)), i, this.props.row.length - 1, offset);
-            }
-            else {
-                partitionPoints = this.transformToPolygonParallel(this.props.visStore.groupScale(currCounts), this.props.visStore.groupScale(f.patients.length), i, this.props.row.length - 1, offset);
-                selectedPoints = this.transformToPolygonParallel(this.props.visStore.groupScale(currCounts), this.props.visStore.groupScale(this.getSelected(f.patients)), i, this.props.row.length - 1, offset);
-            }
-            rects.push(<polygon
-                points={partitionPoints}
-                key={"" + f.key}
-                onMouseEnter={(e) => this.props.showTooltip(e, CategoricalRow.getTooltipContent(f.key, f.patients.length))}
-                onMouseLeave={this.props.hideTooltip}
-                fill={fill} stroke={stroke} opacity={this.props.opacity}/>);
-            if (this.props.uiStore.advancedSelection) {
-                if (this.getSelected(f.patients) > 0) {
-                    rects.push(
-                        <polygon
-                            points={selectedPoints}
-                            key={f.key + 'selected'}
-                            fill='none' stroke='black'/>
-                    );
+                else {
+                    partitionPoints = this.transformToPolygonParallel(this.props.visStore.groupScale(currCounts), this.props.visStore.groupScale(f.patients.length), i, this.props.row.length - 1, offset);
+                    selectedPoints = this.transformToPolygonParallel(this.props.visStore.groupScale(currCounts), this.props.visStore.groupScale(this.getSelected(f.patients)), i, this.props.row.length - 1, offset);
                 }
-            }
-            currCounts += f.patients.length
-        });
+                rects.push(<polygon
+                    points={partitionPoints}
+                    key={"" + f.key}
+                    onMouseEnter={(e) => this.props.showTooltip(e, CategoricalRow.getTooltipContent(f.key, f.patients.length))}
+                    onMouseLeave={this.props.hideTooltip}
+                    fill={fill} stroke={stroke} opacity={this.props.opacity}/>);
+                if (this.props.uiStore.advancedSelection) {
+                    if (this.getSelected(f.patients) > 0) {
+                        rects.push(
+                            <polygon
+                                points={selectedPoints}
+                                key={f.key + 'selected'}
+                                fill='none' stroke='black'/>
+                        );
+                    }
+                }
+                currCounts += f.patients.length
+            });
+        }
+        else {
+            this.props.row.forEach((row) => {
+                let fill = this.props.color(row.key);
+                let stroke = this.props.stroke;
+                if (row.key === undefined) {
+                    if (stroke === "none") {
+                        stroke = "lightgray";
+                    }
+                    fill = "white"
+                }
+                const height = row.patients.length / this.props.patients.length * (this.props.height-1);
+                const y = currCounts / this.props.patients.length * (this.props.height-1);
+                rects.push(<rect key={""+row.key} x="0" y={y} height={height}
+                                 width={this.props.visStore.groupScale(this.props.patients.length)} fill={fill}
+                                 stroke={stroke}
+                                 onMouseEnter={(e) => this.props.showTooltip(e, CategoricalRow.getTooltipContent(row.key, row.patients.length))}
+                                 onMouseLeave={this.props.hideTooltip}/>);
+                currCounts += row.patients.length
+            })
+        }
         return rects
     }
 
@@ -126,6 +159,9 @@ const CategoricalRow = inject("dataStore", "uiStore", "visStore")(observer(class
         let x1, x2, x3, x4, y1, y2, y3, y4;
         y1 = y2 = 0;
         y3 = y4 = this.props.height;
+        if (width / 2 < offset) {
+            offset = width / 2
+        }
         if (index === 0) {
             x1 = x;
             x4 = x;
@@ -148,6 +184,37 @@ const CategoricalRow = inject("dataStore", "uiStore", "visStore")(observer(class
             else {
                 x3 = x + width - offset;
                 x2 = x + width + offset;
+            }
+        }
+        return x1 + "," + y1 + " " + x2 + "," + y2 + " " + x3 + "," + y3 + " " + x4 + "," + y4;
+    }
+
+    transformToPolygonRandom(x, width, index, maxIndex, previousOffset, newOffset) {
+        let x1, x2, x3, x4, y1, y2, y3, y4;
+        y1 = y2 = 0;
+        y3 = y4 = this.props.height;
+        if (index === 0) {
+            x1 = x;
+            x4 = x;
+            if (index === maxIndex) {
+                x2 = x3 = x + width;
+
+            }
+            else {
+                x3 = x + width + newOffset;
+                x2 = x + width - newOffset;
+            }
+        }
+        else {
+            x1 = x - previousOffset;
+            x4 = x + previousOffset;
+            if (index === maxIndex) {
+                x3 = x + width;
+                x2 = x + width;
+            }
+            else {
+                x3 = x + width + newOffset;
+                x2 = x + width - newOffset;
             }
         }
         return x1 + "," + y1 + " " + x2 + "," + y2 + " " + x3 + "," + y3 + " " + x4 + "," + y4;
