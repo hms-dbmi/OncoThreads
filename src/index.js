e275241eba87e72b6d2ee3470b5ebe67629b6797
wangqianwen0418 <wangqianwen0418@163.com>
2020-04-19 20:59:56 +0800

    different time resolution

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'mobx-react';
+import 'antd/dist/antd.css';
 
 import App from './modules/TemporalHeatmap/components/App.jsx';
 import RootStore from './modules/RootStore';
--- src/modules/TemporalHeatmap/components/PlotLabeling/TimeVarConfig.jsx
+++ src/modules/TemporalHeatmap/components/PlotLabeling/TimeVarConfig.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { inject, observer } from 'mobx-react';
-import { Radio } from 'react-bootstrap';
+import { Radio, InputNumber } from 'antd';
 
 /**
  * Legend Component
@@ -9,23 +9,58 @@ const TimeVarConfig = inject('rootStore', 'uiStore')(observer(class TimeVarConfi
     constructor() {
         super();
         this.handleClick = this.handleClick.bind(this);
+        this.handleChangeNumber = this.handleChangeNumber.bind(this);
         
     }
 
-    handleClick(id, value) {
-        this.props.rootStore.setTimeData(id,value)
+    handleClick(e) {
+        console.info(e)
+        this.props.rootStore.setTimeData(e.target.value, e.target.label)
+    }
+    handleChangeNumber(num) {
+        // console.info(e)
+        this.props.rootStore.setTimeData(num, num+'days')
     }
 
     render() {
-        
+        const radioStyle = {
+            display: 'block',
+            height: '30px',
+            lineHeight: '30px',
+          };
         
         return (
             
             <div
             className="menu">
-            <Radio name="groupOptions" onClick={e => this.handleClick("1", "Days")}>Days</Radio>
+            {/* <Radio name="groupOptions" onClick={e => this.handleClick("1", "Days")}>Days</Radio>
+            <Radio name="groupOptions" onClick={e => this.handleClick("7", "Weeks")}>Weeks</Radio>
             <Radio name="groupOptions" onClick={e => this.handleClick("30", "Months")}>Months</Radio>
-            <Radio name="groupOptions" onClick={e => this.handleClick("365", "Years")}>Years</Radio>
+            <Radio name="groupOptions" onClick={e => this.handleClick("365", "Years")}>Years</Radio> */}
+
+            <Radio.Group onChange={this.handleClick} defaultValue={1}>
+                <Radio style={radioStyle} value={1/3600} label="minutes">
+                    minute
+                </Radio>
+                <Radio style={radioStyle} value={1/60} label="hours">
+                    hour
+                </Radio>
+                <Radio style={radioStyle} value={1} label="days">
+                    day
+                </Radio>
+                <Radio style={radioStyle} value={7} label="weeks">
+                    week
+                </Radio>
+                <Radio style={radioStyle} value={30} label="months">
+                    months
+                </Radio>
+                <Radio style={radioStyle} value={365} label="years">
+                    years
+                </Radio>
+            </Radio.Group>
+            <br/>
+            Customize a time range <br/>
+            <InputNumber min={1} max={100} defaultValue={1} onChange={this.handleChangeNumber}  /> days
         </div>
         );
     }
