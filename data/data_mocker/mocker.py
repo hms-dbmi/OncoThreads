78726c3c5ebfdb00fad278c504ad9ae4032f1771
wangqianwen0418 <wangqianwen0418@163.com>
2020-08-06 19:32:18 -0400

    enable state temporal distribution

--- data/data_mocker/mocker.py
+++ data/data_mocker/mocker.py
@@ -1,13 +1,17 @@
 import random
+import math
+import numpy as np
+from sklearn.datasets import make_blobs
 import os
 os.chdir(os.path.dirname(__file__))
 
 
 PATIENT_NUM = 60
 TIMEPOINT_NUM = 10
-STATUS_DIM = 10
+STATUS_DIM = 8
+STATUS_DIM_RANDOM = 2 # number of noise status dimensions
 EVENT_DIM = 8
-STATE_NUM = 6
+STATE_NUM = 5
 
 treatment_types = ['treat_'+chr(65+i) for i in range(EVENT_DIM)]
 
@@ -58,25 +62,56 @@ sample_header = [
 
 sample_file.writelines(sample_header)
 
-from sklearn.datasets import make_blobs
-sample_num = sum(patient_len_dict.values())
 
-X, _ = make_blobs(n_samples=sample_num, centers=STATE_NUM, n_features=STATUS_DIM)
+sample_num = sum(patient_len_dict.values())
+X, y = make_blobs(n_samples=sample_num, centers=STATE_NUM, n_features=STATUS_DIM-STATUS_DIM_RANDOM)
+
+# sample time distribution (linear mocker)
+def GET_STATE_RANGE(left_min=0, left_max=TIMEPOINT_NUM, right_min=0, right_max=STATE_NUM): 
+    # Figure out how 'wide' each range is  
+    leftSpan = left_max - left_min   
+    rightSpan = right_max - right_min 
+
+    # Compute the scale factor between left and right values 
+    scaleFactor = float(rightSpan) / float(leftSpan) 
+
+    # create interpolation function using pre-calculated scaleFactor
+    def get_state_range(value):
+        state_estimate = math.floor(right_min + (value-left_min)*scaleFactor)
+        return [
+            state_estimate, 
+            min(STATE_NUM-1, state_estimate+1)
+            ]
+
+    return get_state_range
+
+get_state_range = GET_STATE_RANGE()
 
 
 sample_idx = 0
 for patient_idx in range(PATIENT_NUM):
     patient_id = 'patient_'+str(patient_idx)
-    for time_id in range(patient_len_dict[patient_id]):
+    for time_idx in range(patient_len_dict[patient_id]):
         sample_id = 'sample_'+str(sample_idx)
         points_file.write(
                 "{patient_id}\t{start}\tSPECIMEN\t{sample_id}\n".
                 format(
-                    patient_id=patient_id, start=time_id, sample_id = sample_id
+                    patient_id=patient_id, start=time_idx, sample_id = sample_id
                 )
             ) 
+        flag = 0
 
-        sample_status = [str(i) for i in X[sample_idx]]
+        while not get_state_range(time_idx)[0]<= y[flag] <= get_state_range(time_idx)[1]:
+            if flag<len(X)-1: 
+                flag += 1
+            else:
+                break
+
+        
+        suit_sample = X[flag]
+        X = np.delete(X, flag, 0) 
+        y = np.delete(y, flag, 0)
+        sample_status = [str(i) for i in suit_sample] + [str(random.uniform(-2,2)) for _ in range(STATUS_DIM_RANDOM)]
         sample_file.write(
             '{patient_id}\t{sample_id}\t{status}\n'.format(
                 patient_id = patient_id, sample_id = sample_id, status = '\t'.join(sample_status)
