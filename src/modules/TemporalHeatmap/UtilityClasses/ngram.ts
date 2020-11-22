d2770b9b0a388ded553fcc7f2319f5f50dfac680
wangqianwen0418 <wangqianwen0418@163.com>
2020-11-22 11:44:32 -0500

    ngram func

--- src/modules/TemporalHeatmap/UtilityClasses/ngram.ts
+++ src/modules/TemporalHeatmap/UtilityClasses/ngram.ts
@@ -1,7 +1,8 @@
 type TCounts = {
     [key:string]:{
         original:string[],
-        counts?: number[], // counts at each sequence
+        // counts?: number[], // counts at each sequence
+        seqCounts: number[],
         totalCount:number, // 
     }
 }
@@ -9,15 +10,18 @@ type TCounts = {
 const count_arrays = ( arrs:string[][], n:number, minSupport:number, debug:boolean ) => {
     const counts:TCounts = {};
     if (debug) console.log("arr:", arrs);
-    arrs.forEach(arr=>{
+    arrs.forEach((arr,arrIdx)=>{
         for (let i = n; i <= arr.length; i++) {
             const ngram = arr.slice(i - n, i);
             if (debug) console.log("ngram:", ngram);
             let gramKey = ngram.join()
             if (counts.hasOwnProperty(gramKey)) {
                 counts[gramKey].totalCount++;
+                counts[gramKey]['seqCounts'][arrIdx] += 1
             } else {
-                counts[gramKey] = { original: ngram, totalCount: 1 };
+                let seqCounts = arrs.map(_=>0)
+                seqCounts[arrIdx] = 1
+                counts[gramKey] = { original: ngram, totalCount: 1, seqCounts};
             }
         }
     })
@@ -34,6 +38,7 @@ const count_arrays = ( arrs:string[][], n:number, minSupport:number, debug:boole
 
 
 const n_gram=(data:string[][], ns:number[], minSupport:number, debug:boolean=false ) => {
+    
     let results:(number|string[])[][] = []
     ns.forEach(n=>{
         let grams = count_arrays( data, n, minSupport, debug );
