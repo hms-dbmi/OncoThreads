5113c172e650795aad42a2150f322804d91f567f
wangqianwen0418 <wangqianwen0418@163.com>
2021-01-31 11:52:48 -0500

    refactor: pattern table

---
+++ src/modules/TemporalHeatmap/components/StateTransition/TransitionOverview/PatternTable.tsx
@@ -0,0 +1,164 @@
+import React from "react"
+import { observer, inject } from 'mobx-react';
+import { IRootStore } from "modules/Type";
+import { Table, Input, Button, Space, Checkbox, Tooltip } from 'antd';
+import { getColorByName } from 'modules/TemporalHeatmap/UtilityClasses/'
+import { SearchOutlined, InfoCircleOutlined } from '@ant-design/icons';
+import { ColumnsType } from 'antd/lib/table'
+import { TPattern } from "modules/TemporalHeatmap/UtilityClasses/prefixSpan";
+
+type RowRecordType = { key: string, pattern: TPattern, [key: string]: any }
+
+interface Props {
+    rootStore?: IRootStore,
+    height: number,
+    annotationWidth:number,
+    paddingW:number
+}
+
+interface State {
+    searchedPatternLengths: number[]
+}
+
+@inject('rootStore')
+@observer
+class PatternTable extends React.Component <Props, State> {
+    searchInput: Input | null = null;
+    constructor(props: Props) {
+        super(props)
+        this.state = {
+            searchedPatternLengths: [2, 3]
+        }
+    }
+    frequentPatternTable() {
+        let { dataStore } = this.props.rootStore!
+
+        let { ngramResults, frequentPatterns, patientGroups, encodingMetric } = dataStore
+        if (encodingMetric === "ngram") {
+            frequentPatterns = ngramResults
+        }
+        let rectW = 10
+
+        const handleSearch = (selectedKeys: string[], confirm: () => void, dataIndex: string) => {
+            confirm();
+
+        };
+
+        const handleReset = (clearFilters: () => void) => {
+            clearFilters()
+        };
+
+        const changePatternLength = (len: number) => {
+            let { searchedPatternLengths } = this.state
+            let idx = searchedPatternLengths.indexOf(len)
+            if (idx > -1) {
+                searchedPatternLengths.splice(idx, 1)
+            } else {
+                searchedPatternLengths.push(len)
+            }
+
+            this.setState({ searchedPatternLengths })
+        }
+
+        const getColumnSearchProps = (dataIndex: string) => ({
+            filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }: { setSelectedKeys: any, selectedKeys: string[], confirm: () => void, clearFilters: () => void }) => (
+                <div style={{ padding: 8 }}>
+                    contains: <Input
+                        ref={node => {
+                            this.searchInput = node;
+                        }}
+                        placeholder={`Search ${dataIndex}`}
+                        value={selectedKeys[0]}
+                        onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+                        onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+                        style={{ width: 188, marginBottom: 8, display: 'block' }}
+                    />
+                    <Checkbox checked={this.state.searchedPatternLengths.includes(2)} onChange={() => changePatternLength(2)} /> two-state pattern
+                    <br />
+                    <Checkbox checked={this.state.searchedPatternLengths.includes(3)} onChange={() => changePatternLength(3)} /> three-state pattern
+                    <br />
+                    <Space>
+                        <Button
+                            type="primary"
+                            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+                            icon={<SearchOutlined translate='(0,0)' />}
+                            size="small"
+                            style={{ width: 90 }}
+                        >
+                            Search
+                  </Button>
+                        <Button onClick={() => handleReset(clearFilters)} size="small" style={{ width: 90 }}>
+                            Reset
+                  </Button>
+                    </Space>
+                </div>
+            ),
+            filterIcon: (filtered: boolean) => <SearchOutlined translate='(0,0)' style={{ color: filtered ? '#1890ff' : undefined }} />,
+            onFilter: (value: string | number | boolean, record: RowRecordType): boolean =>
+                record[dataIndex]
+                    ? record[dataIndex].join('').toLowerCase().includes(value.toString().replace(/\s|,/g, '').toLowerCase())
+                    &&
+                    this.state.searchedPatternLengths.includes(record[dataIndex].length)
+                    : false,
+            onFilterDropdownVisibleChange: (visible: boolean) => {
+                if (visible) {
+                    setTimeout(() => this.searchInput!.select(), 100);
+                }
+            },
+        });
+
+
+        let data = frequentPatterns.map((pattern, patternIdx) => {
+            let [supportIdxs, subseq] = pattern
+
+            let rowData: RowRecordType = {
+                key: `${patternIdx + 1}`,
+                pattern: subseq,
+            }
+
+            patientGroups.forEach((patientGroup, groupIdx) => {
+                let groupSupportIdxs = supportIdxs.filter(p => patientGroup.includes(p))
+                let percentage = groupSupportIdxs.length === 0 ? '0%' : Math.floor(groupSupportIdxs.length / patientGroup.length * 100).toString() + '%'
+                rowData[`group_${groupIdx}`] = percentage
+            })
+
+            return rowData
+        })
+
+        let columns: ColumnsType<RowRecordType> = patientGroups.map((_, groupIdx) => {
+            return {
+                title: `group_${groupIdx}`,
+                dataIndex: `group_${groupIdx}`,
+                key: `group_${groupIdx}`,
+                sorter: (a, b) => parseInt(a[`group_${groupIdx}`].replace('%', '')) - parseInt(b[`group_${groupIdx}`].replace('%', '')),
+                align: 'center',
+                // width: groupIdx>0?this.groupLabelOffsetX[groupIdx] - this.groupLabelOffsetX[groupIdx-1] : this.groupLabelOffsetX[groupIdx]
+            }
+        })
+
+        columns.unshift({
+            title: '',
+            dataIndex: 'pattern',
+            key: 'pattern',
+            render: (states: string[]) => {
+                return states.map((state, stateIdx) => {
+                    return <div key={`${states}+${stateIdx}+${state}`} style={{ width: rectW, margin: 1, backgroundColor: getColorByName(state), fontSize: rectW, color: "white" }} >{state}</div>
+                })
+            },
+            ...getColumnSearchProps('pattern'),
+            align: 'center',
+            width: this.props.annotationWidth + this.props.paddingW
+        })
+
+
+        let tableHeader = <span>Frequent Patterns {' '}<Tooltip title="frequent state transition patterns and their distribution of each patient group"><InfoCircleOutlined translate='' />
+        </Tooltip>
+        </span>
+        return <Table columns={columns} dataSource={data} pagination={false} scroll={{ y: this.props.height }} title={() => tableHeader} />
+    }
+    render(){
+        return this.frequentPatternTable()
+    }
+}
+
+export default PatternTable
--- src/modules/TemporalHeatmap/components/StateTransition/TransitionOverview/index.tsx
+++ src/modules/TemporalHeatmap/components/StateTransition/TransitionOverview/index.tsx
@@ -3,12 +3,11 @@ import * as d3 from "d3"
 import { observer, inject } from 'mobx-react';
 import { IRootStore } from "modules/Type";
 import { getColorByName, getTextWidth } from 'modules/TemporalHeatmap/UtilityClasses/'
-import { Table, Input, Button, Space, Checkbox, Tooltip } from 'antd';
-import { SearchOutlined, InfoCircleOutlined } from '@ant-design/icons';
-import { ColumnsType } from 'antd/lib/table'
+import { Input, Checkbox} from 'antd';
 import { TPattern } from "modules/TemporalHeatmap/UtilityClasses/prefixSpan";
 
 import GridLayout from 'react-grid-layout';
+import PatternTable from './PatternTable'
 
 interface Props {
     rootStore?: IRootStore,
@@ -274,141 +273,6 @@ class TransitionOverview extends React.Component<Props, State> {
 
 
 
-    frequentPatternTable() {
-        let { dataStore } = this.props.rootStore!
-
-        let { ngramResults, frequentPatterns, patientGroups } = dataStore
-        if (dataStore.encodingMetric === "ngram") {
-            frequentPatterns = ngramResults
-        }
-        let rectW = 10
-
-        const handleSearch = (selectedKeys: string[], confirm: () => void, dataIndex: string) => {
-            confirm();
-
-        };
-
-        const handleReset = (clearFilters: () => void) => {
-            clearFilters()
-        };
-
-        const changePatternLength = (len: number) => {
-            let { searchedPatternLengths } = this.state
-            let idx = searchedPatternLengths.indexOf(len)
-            if (idx > -1) {
-                searchedPatternLengths.splice(idx, 1)
-            } else {
-                searchedPatternLengths.push(len)
-            }
-
-            this.setState({ searchedPatternLengths })
-        }
-
-        const getColumnSearchProps = (dataIndex: string) => ({
-            filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }: { setSelectedKeys: any, selectedKeys: string[], confirm: () => void, clearFilters: () => void }) => (
-                <div style={{ padding: 8 }}>
-                    contains: <Input
-                        ref={node => {
-                            this.searchInput = node;
-                        }}
-                        placeholder={`Search ${dataIndex}`}
-                        value={selectedKeys[0]}
-                        onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
-                        onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
-                        style={{ width: 188, marginBottom: 8, display: 'block' }}
-                    />
-                    <Checkbox checked={this.state.searchedPatternLengths.includes(2)} onChange={() => changePatternLength(2)} /> two-state pattern
-                    <br />
-                    <Checkbox checked={this.state.searchedPatternLengths.includes(3)} onChange={() => changePatternLength(3)} /> three-state pattern
-                    <br />
-                    <Space>
-                        <Button
-                            type="primary"
-                            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
-                            icon={<SearchOutlined translate='(0,0)' />}
-                            size="small"
-                            style={{ width: 90 }}
-                        >
-                            Search
-                  </Button>
-                        <Button onClick={() => handleReset(clearFilters)} size="small" style={{ width: 90 }}>
-                            Reset
-                  </Button>
-                    </Space>
-                </div>
-            ),
-            filterIcon: (filtered: boolean) => <SearchOutlined translate='(0,0)' style={{ color: filtered ? '#1890ff' : undefined }} />,
-            onFilter: (value: string | number | boolean, record: RowRecordType): boolean =>
-                record[dataIndex]
-                    ? record[dataIndex].join('').toLowerCase().includes(value.toString().replace(/\s|,/g, '').toLowerCase())
-                    &&
-                    this.state.searchedPatternLengths.includes(record[dataIndex].length)
-                    : false,
-            onFilterDropdownVisibleChange: (visible: boolean) => {
-                if (visible) {
-                    setTimeout(() => this.searchInput!.select(), 100);
-                }
-            },
-            // render: text =>(
-            //     <Highlighter
-            //       highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
-            //       searchWords={[this.state.searchText]}
-            //       autoEscape
-            //       textToHighlight={text ? text.toString() : ''}
-            //     />
-            //   ) 
-        });
-
-
-        let data = frequentPatterns.map((pattern, patternIdx) => {
-            let [supportIdxs, subseq] = pattern
-
-            let rowData: RowRecordType = {
-                key: `${patternIdx + 1}`,
-                pattern: subseq,
-            }
-
-            patientGroups.forEach((patientGroup, groupIdx) => {
-                let groupSupportIdxs = supportIdxs.filter(p => patientGroup.includes(p))
-                let percentage = groupSupportIdxs.length === 0 ? '0%' : Math.floor(groupSupportIdxs.length / patientGroup.length * 100).toString() + '%'
-                rowData[`group_${groupIdx}`] = percentage
-            })
-
-            return rowData
-        })
-
-        let columns: ColumnsType<RowRecordType> = patientGroups.map((_, groupIdx) => {
-            return {
-                title: `group_${groupIdx}`,
-                dataIndex: `group_${groupIdx}`,
-                key: `group_${groupIdx}`,
-                sorter: (a, b) => parseInt(a[`group_${groupIdx}`].replace('%', '')) - parseInt(b[`group_${groupIdx}`].replace('%', '')),
-                align: 'center',
-                // width: groupIdx>0?this.groupLabelOffsetX[groupIdx] - this.groupLabelOffsetX[groupIdx-1] : this.groupLabelOffsetX[groupIdx]
-            }
-        })
-
-        columns.unshift({
-            title: '',
-            dataIndex: 'pattern',
-            key: 'pattern',
-            render: (states: string[]) => {
-                return states.map((state, stateIdx) => {
-                    return <div key={`${states}+${stateIdx}+${state}`} style={{ width: rectW, margin: 1, backgroundColor: getColorByName(state), fontSize: rectW, color: "white" }} >{state}</div>
-                })
-            },
-            ...getColumnSearchProps('pattern'),
-            align: 'center',
-            width: this.annotationWidth + this.paddingW
-        })
-
-
-        let tableHeader = <span>Frequent Patterns {' '}<Tooltip title="frequent state transition patterns and their distribution of each patient group"><InfoCircleOutlined translate='' />
-        </Tooltip>
-        </span>
-        return <Table columns={columns} dataSource={data} pagination={false} scroll={{ y: this.props.height * 0.3 }} title={() => tableHeader} />
-    }
-
     render() {
         let overviewHeight = this.paddingH + this.groupLabelHeight + this.props.rootStore!.dataStore.timepoints.filter(d => d.type === "sample").length * this.timeStepHeight + this.rectHeight
         const layout = [
@@ -443,7 +307,7 @@ class TransitionOverview extends React.Component<Props, State> {
             <div key='table'
                 data-intro={dataIntro2}
                 data-step='4'>
-                {this.frequentPatternTable()}
+                <PatternTable annotationWidth={this.annotationWidth} paddingW={this.paddingW} height={this.props.height*0.3}/>
             </div>
         </GridLayout>
 
