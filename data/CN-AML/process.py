c552e9659ae0a65753eeec9170028331d3174ae6
wangqianwen0418 <wangqianwen0418@163.com>
2021-03-09 12:37:57 -0500

    ignore data files but keep process py

---
+++ data/CN-AML/process.py
@@ -0,0 +1,109 @@
+'''
+data is available at the supplementary material of https://clincancerres.aacrjournals.org/content/24/7/1716.long
+'''
+# %%
+import pandas as pd
+
+# %%
+'''
+process patient data
+'''
+target_folder = "processed_data"
+filename = 'patients.txt'
+
+patients_df = pd.read_excel(
+    'Detailed patient characteristics.xlsx', index_col=None)
+
+patient_features = ['UPN', 'Gender', 'Age at Diagnosis',
+                    'Time to Relapse', 'AML Type', 'ELN Risk Group']
+rename_dict = {
+    "UPN": "PATIENT_ID",
+    'Age at Diagnosis': 'age',
+    'Gender': 'sex',
+    'Time to Relapse': 'Relapse Date',
+    'AML Type': 'AML',
+    'ELN Risk Group': 'ELN RisK'
+}
+patient_renamed_features = [rename_dict[k] for k in patient_features]
+
+saved_patients = patients_df[patient_features].rename(columns=rename_dict)
+
+patient_header = [
+    "#" + '\t'.join(patient_renamed_features) + '\n',
+    "#" + '\t'.join(patient_renamed_features) + '\n',
+    "#" + '\t'.join(['STRING', 'STRING', 'NUMBER',
+                     'NUMBER', 'STRING', 'STRING']) + '\n',
+    "#" + '\t'.join(['1' for _ in range(len(patient_renamed_features))])+'\n',
+]
+
+with open('processed_data/AML_patients.txt', 'w') as txt_file:
+    for line in patient_header:
+        txt_file.write(line)
+
+    saved_patients[patient_renamed_features].to_csv(
+        txt_file, index=False, sep='\t', na_rep=' ')
+
+
+# %%
+'''
+timeline and samples
+'''
+mutation_df = pd.read_excel(
+    'Somatic variants.xlsx', index_col=None)
+
+mutations = ['DNMT3A', 'FLT3', 'NPM1', 'IDH2', 'IDH1']
+timepoints = ['% VAF* Dx', '% VAF* Rem', '% VAF* Rel']
+
+sample_df = pd.DataFrame(
+    columns=['PATIENT_ID', 'SAMPLE_ID']+mutations
+)
+timeline_df = pd.DataFrame(
+    columns=['PATIENT_ID', 'START_DATE', 'EVENT_TYPE', 'SAMPLE_ID']
+)
+for patient_id in patients_df['UPN']:
+    if not pd.isna(patient_id):
+        sub_df = mutation_df[mutation_df.UPN == patient_id]
+        for tp in timepoints:
+            sample_id = len(sample_df)
+            sample = [patient_id, sample_id]
+            for mutation in mutations:
+                if mutation in sub_df.Gene.to_list():
+                    value = sub_df[sub_df.Gene == mutation][tp].to_list()[0]
+                    if value == '-':
+                        sample.append(0)
+                    else:
+                        sample.append(value)
+                else:
+                    sample.append(0)
+            sample_df = sample_df.append(
+                pd.Series(sample, index=sample_df.columns), ignore_index=True)
+
+            timeline_df = timeline_df.append({
+                'PATIENT_ID': patient_id,
+                'START_DATE': timepoints.index(tp),
+                'EVENT_TYPE': 'SPECIMEN',
+                'SAMPLE_ID': sample_id},
+                ignore_index=True)
+
+
+# %%
+timeline_df.to_csv('processed_data/AML_timeline.txt',
+                   index=False, sep='\t')
+
+sample_header = [
+    "#" + '\t'.join(['Patient_Identifier',
+                     'Sample_Identifier'] + mutations) + '\n',
+    "#" + '\t'.join(['Patient_Identifier',
+                     'Sample_Identifier'] + mutations) + '\n',
+    "#" + '\t'.join(['STRING', 'STRING'] +
+                    ['NUMBER' for key in mutations]) + '\n',
+    "#" + '\t'.join(['1' for _ in range(2 + len(mutations))])+'\n',
+]
+
+with open('processed_data/AML_obs.txt', 'w') as txt_file:
+    for line in sample_header:
+        txt_file.write(line)
+    sample_df.to_csv(txt_file, index=False, sep='\t', na_rep=' ')
+
+
+# %%
--- data/synthea/process.py
+++ data/synthea/process.py
@@ -1,4 +1,7 @@
-#%%
+'''
+data is available at https://synthea.mitre.org/downloads
+'''
+# %%
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,27 +9,27 @@ import matplotlib.ticker as ticker
 import seaborn as sns
 import datetime
 
-#%%
+# %%
 '''
 loading data
 '''
 
-SAMPLE_SIZE = 100 # number of sampled inpatients
+SAMPLE_SIZE = 100  # number of sampled inpatients
 observation_code = {
-    '48065-7': 
-    {'name':'D-dimer', 'type': ''}, #
-    '2276-4': 
-    {'name':'Serum Ferritin', 'type':''},#
-    '89579-7': 
-    {'name': 'High Sensitivity Cardiac Troponin I', 'type':''},
-    '14804-9': 
-    {'name':'Lactate dehydrogenase', 'type': ''},#
+    '48065-7':
+    {'name': 'D-dimer', 'type': ''},
+    '2276-4':
+    {'name': 'Serum Ferritin', 'type': ''},
+    '89579-7':
+    {'name': 'High Sensitivity Cardiac Troponin I', 'type': ''},
+    '14804-9':
+    {'name': 'Lactate dehydrogenase', 'type': ''},
     '9279-1':
     {'name': 'Respiratory rate', 'type': ''},
     '38483-4':
     {'name': 'Creatinine', 'type': ''},
     '26464-8':
-    {'name':'white blood cell', 'type': ''},
+    {'name': 'white blood cell', 'type': ''},
     # '26453-1':
     # {'name':'red blood cell', 'type':''},
     '1988-5':
@@ -40,20 +43,28 @@ observation_code = {
 }
 TIMEPOINT_ATTR_CODES = [key for key in observation_code]
 
-conditions = pd.read_csv(os.path.join('./originalData/10k_synthea_covid19_csv',"conditions.csv"))
-patients = pd.read_csv(os.path.join('./originalData/10k_synthea_covid19_csv',"patients.csv"))
-observations = pd.read_csv(os.path.join('./originalData/10k_synthea_covid19_csv',"observations.csv"))
-care_plans = pd.read_csv(os.path.join('./originalData/10k_synthea_covid19_csv',"careplans.csv"))
-encounters = pd.read_csv(os.path.join('./originalData/10k_synthea_covid19_csv',"encounters.csv"))
-devices = pd.read_csv(os.path.join('./originalData/10k_synthea_covid19_csv',"devices.csv"))
-supplies = pd.read_csv(os.path.join('./originalData/10k_synthea_covid19_csv','supplies.csv'))
-procedures = pd.read_csv(os.path.join('./originalData/10k_synthea_covid19_csv',"procedures.csv"))
-medications = pd.read_csv(os.path.join('./originalData/10k_synthea_covid19_csv',"medications.csv"))
+conditions = pd.read_csv(os.path.join(
+    './originalData/10k_synthea_covid19_csv', "conditions.csv"))
+patients = pd.read_csv(os.path.join(
+    './originalData/10k_synthea_covid19_csv', "patients.csv"))
+observations = pd.read_csv(os.path.join(
+    './originalData/10k_synthea_covid19_csv', "observations.csv"))
+care_plans = pd.read_csv(os.path.join(
+    './originalData/10k_synthea_covid19_csv', "careplans.csv"))
+encounters = pd.read_csv(os.path.join(
+    './originalData/10k_synthea_covid19_csv', "encounters.csv"))
+devices = pd.read_csv(os.path.join(
+    './originalData/10k_synthea_covid19_csv', "devices.csv"))
+supplies = pd.read_csv(os.path.join(
+    './originalData/10k_synthea_covid19_csv', 'supplies.csv'))
+procedures = pd.read_csv(os.path.join(
+    './originalData/10k_synthea_covid19_csv', "procedures.csv"))
+medications = pd.read_csv(os.path.join(
+    './originalData/10k_synthea_covid19_csv', "medications.csv"))
 
 
-
-#%%
-# pd.set_option("max_rows", None) 
+# %%
+# pd.set_option("max_rows", None)
 # print([col for col in observations.columns])
 # #  ['DATE', 'PATIENT', 'ENCOUNTER', 'CODE', 'DESCRIPTION', 'VALUE', 'UNITS', 'TYPE']
 # print(observations.drop_duplicates('CODE')\
@@ -67,17 +78,17 @@ medications = pd.read_csv(os.path.join('./originalData/10k_synthea_covid19_csv',
 # # ['START', 'STOP', 'PATIENT', 'ENCOUNTER', 'CODE', 'DESCRIPTION']
 
 # print([col for col in patients.columns])
-# # ['Id', 'BIRTHDATE', 'DEATHDATE', 'SSN', 'DRIVERS', 'PASSPORT', 'PREFIX', 'FIRST', 'LAST', 
-# # 'SUFFIX', 'MAIDEN', 'MARITAL', 'RACE', 'ETHNICITY', 'GENDER', 'BIRTHPLACE', 'ADDRESS', 
+# # ['Id', 'BIRTHDATE', 'DEATHDATE', 'SSN', 'DRIVERS', 'PASSPORT', 'PREFIX', 'FIRST', 'LAST',
+# # 'SUFFIX', 'MAIDEN', 'MARITAL', 'RACE', 'ETHNICITY', 'GENDER', 'BIRTHPLACE', 'ADDRESS',
 # # 'CITY', 'STATE', 'COUNTY', 'ZIP', 'LAT', 'LON', 'HEALTHCARE_EXPENSES', 'HEALTHCARE_COVERAGE']
 
 # print([col for col in encounters.columns])
-# # ['Id', 'START', 'STOP', 'PATIENT', 'ORGANIZATION', 'PROVIDER', 'PAYER', 'ENCOUNTERCLASS', 'CODE', 
-# # 'DESCRIPTION', 'BASE_ENCOUNTER_COST', 'TOTAL_CLAIM_COST', 'PAYER_COVERAGE', 'REASONCODE', 
+# # ['Id', 'START', 'STOP', 'PATIENT', 'ORGANIZATION', 'PROVIDER', 'PAYER', 'ENCOUNTERCLASS', 'CODE',
+# # 'DESCRIPTION', 'BASE_ENCOUNTER_COST', 'TOTAL_CLAIM_COST', 'PAYER_COVERAGE', 'REASONCODE',
 # # 'REASONDESCRIPTION']
 
 # print([col for col in medications.columns])
-#%%
+# %%
 '''
 get patient ids
 '''
@@ -85,22 +96,26 @@ get patient ids
 
 covid_patient_ids = conditions[conditions.CODE == 840539006].PATIENT.unique()
 
-negative_covid_patient_ids = observations[(observations.CODE == '94531-1') & (observations.VALUE == 'Not detected (qualifier value)')].PATIENT.unique()
+negative_covid_patient_ids = observations[(observations.CODE == '94531-1') & (
+    observations.VALUE == 'Not detected (qualifier value)')].PATIENT.unique()
 
 # that is more thatn thoese died because of COVID
 deceased_patients = patients[patients.DEATHDATE.notna()].Id
 
-completed_isolation_patients = care_plans[(care_plans.CODE == 736376001) & (care_plans.STOP.notna()) & (care_plans.REASONCODE == 840539006)].PATIENT
+completed_isolation_patients = care_plans[(care_plans.CODE == 736376001) & (
+    care_plans.STOP.notna()) & (care_plans.REASONCODE == 840539006)].PATIENT
 
 # Survivors are the union of those who have completed isolation at home or have a negative SARS-CoV-2 test.
-survivor_ids = np.union1d(completed_isolation_patients, negative_covid_patient_ids)
+survivor_ids = np.union1d(completed_isolation_patients,
+                          negative_covid_patient_ids)
 
 #  Grab IDs for patients with admission due to COVID-19
-inpatient_ids = encounters[(encounters.REASONCODE == 840539006) & (encounters.CODE == 1505002)].PATIENT
+inpatient_ids = encounters[(encounters.REASONCODE == 840539006) & (
+    encounters.CODE == 1505002)].PATIENT
 
 
 # # sample inpatinets
-sampled_patient_ids = inpatient_ids.sample(n = SAMPLE_SIZE)
+sampled_patient_ids = inpatient_ids.sample(n=SAMPLE_SIZE)
 # sampled_patient_ids = pd.DataFrame(np.intersect1d( inpatient_ids, deceased_patients)).sample(n = SAMPLE_SIZE)
 
 # print('number of inpatient', sampled_patient_ids.shape[0])
@@ -110,25 +125,24 @@ sampled_patient_ids = inpatient_ids.sample(n = SAMPLE_SIZE)
 # sample deceased patients
 
 
-
-
-
 '''
 Lab values for COVID-19 patients
 '''
 
 
-#%%
-# 
+# %%
+#
 lab_obs = observations[observations['CODE'].isin(TIMEPOINT_ATTR_CODES)]
 
 # Select COVID-19 conditions out of all conditions in the simulation
 covid_conditions = conditions[conditions.CODE == 840539006]
-covid_conditions = covid_conditions[covid_conditions['PATIENT'].isin(sampled_patient_ids)]
+covid_conditions = covid_conditions[covid_conditions['PATIENT'].isin(
+    sampled_patient_ids)]
 
-#Merge the COVID-19 conditions with the patients
+# Merge the COVID-19 conditions with the patients
 sampled_inpatients = patients[patients['Id'].isin(sampled_patient_ids)]
-covid_patients = covid_conditions.merge(sampled_inpatients, how='left', left_on='PATIENT', right_on='Id')
+covid_patients = covid_conditions.merge(
+    sampled_inpatients, how='left', left_on='PATIENT', right_on='Id')
 
 # Add an attribute to the DataFrame indicating whether this is a survivor or not.
 covid_patients['survivor'] = covid_patients.PATIENT.isin(survivor_ids)
@@ -138,24 +152,32 @@ covid_patients = covid_patients[['START', 'PATIENT', 'survivor', 'CODE']]
 covid_patients_obs = covid_patients.merge(lab_obs, on='PATIENT')
 covid_patients_obs['START'] = pd.to_datetime(covid_patients_obs.START)
 covid_patients_obs['DATE'] = pd.to_datetime(covid_patients_obs.DATE)
-covid_patients_obs['lab_days'] = covid_patients_obs.DATE - covid_patients_obs.START
-covid_patients_obs['days'] = covid_patients_obs.lab_days / np.timedelta64(1, 'D')
-covid_patients_obs = covid_patients_obs[covid_patients_obs.days >= 0].reset_index(drop=True)
-covid_patients_obs['VALUE'] = pd.to_numeric(covid_patients_obs['VALUE'], errors='coerce')
+covid_patients_obs['lab_days'] = covid_patients_obs.DATE - \
+    covid_patients_obs.START
+covid_patients_obs['days'] = covid_patients_obs.lab_days / \
+    np.timedelta64(1, 'D')
+covid_patients_obs = covid_patients_obs[covid_patients_obs.days >= 0].reset_index(
+    drop=True)
+covid_patients_obs['VALUE'] = pd.to_numeric(
+    covid_patients_obs['VALUE'], errors='coerce')
 
 # Reduce the columns on the DataFrame to ones needed
-timeline_specimen = covid_patients_obs.drop_duplicates(subset=['PATIENT', 'days'])[['PATIENT', 'days']].reset_index(drop=True)
-timeline_specimen = timeline_specimen.rename(columns={"PATIENT": "PATIENT_ID", 'days': 'START_DATE'})
+timeline_specimen = covid_patients_obs.drop_duplicates(
+    subset=['PATIENT', 'days'])[['PATIENT', 'days']].reset_index(drop=True)
+timeline_specimen = timeline_specimen.rename(
+    columns={"PATIENT": "PATIENT_ID", 'days': 'START_DATE'})
 timeline_specimen['START_DATE'] = timeline_specimen['START_DATE'].astype('int')
 timeline_specimen['EVENT_TYPE'] = "SPECIMEN"
-timeline_specimen['SAMPLE_ID'] = ['sample_'+str(i) for i in range(len(timeline_specimen))]
+timeline_specimen['SAMPLE_ID'] = ['sample_' +
+                                  str(i) for i in range(len(timeline_specimen))]
 
-#%%
+# %%
 medications = medications[medications['PATIENT'].isin(sampled_patient_ids)]
 
 # covid_med = medications[pd.to_datetime(medications.START) > pd.to_datetime('2020-01-20')]
 
-inpatient_dates = covid_patients_obs[['START', 'PATIENT', 'DATE']].groupby(['PATIENT']).max().rename(columns={'START': 'INPATIENT_START', 'DATE': 'INPATIENT_END'})
+inpatient_dates = covid_patients_obs[['START', 'PATIENT', 'DATE']].groupby(
+    ['PATIENT']).max().rename(columns={'START': 'INPATIENT_START', 'DATE': 'INPATIENT_END'})
 
 covid_med = medications.merge(inpatient_dates, on='PATIENT')
 covid_med['STOP'].fillna(covid_med['INPATIENT_END'])
@@ -163,24 +185,28 @@ covid_med['START'] = pd.to_datetime(covid_med['START'])
 covid_med['STOP'] = pd.to_datetime(covid_med['STOP'])
 covid_med['STOP'] = covid_med[['STOP', 'INPATIENT_END']].max(axis=1)
 
-covid_med = covid_med[ covid_med['START']>=covid_med['INPATIENT_START']]
-covid_med = covid_med[covid_med['START']<=covid_med['INPATIENT_END']]
+covid_med = covid_med[covid_med['START'] >= covid_med['INPATIENT_START']]
+covid_med = covid_med[covid_med['START'] <= covid_med['INPATIENT_END']]
 
-covid_med['START_DATE'] = (covid_med['START'] - covid_med['INPATIENT_START']) / np.timedelta64(1, 'D')
-covid_med['STOP_DATE'] = (covid_med['STOP'] - covid_med['INPATIENT_START']) / np.timedelta64(1, 'D')
-covid_med['EVENT_TYPE'] ='MEDICATION'
-covid_med['MED'] = covid_med['DESCRIPTION'] 
+covid_med['START_DATE'] = (
+    covid_med['START'] - covid_med['INPATIENT_START']) / np.timedelta64(1, 'D')
+covid_med['STOP_DATE'] = (
+    covid_med['STOP'] - covid_med['INPATIENT_START']) / np.timedelta64(1, 'D')
+covid_med['EVENT_TYPE'] = 'MEDICATION'
+covid_med['MED'] = covid_med['DESCRIPTION']
 
-covid_med = covid_med[['PATIENT', 'START_DATE', 'STOP_DATE', 'EVENT_TYPE', 'MED']].rename(columns={"PATIENT": "PATIENT_ID"})
+covid_med = covid_med[['PATIENT', 'START_DATE', 'STOP_DATE',
+                       'EVENT_TYPE', 'MED']].rename(columns={"PATIENT": "PATIENT_ID"})
 
 
 # covid_med['']
-#%%
+# %%
 
 
 samples = pd.DataFrame(
-    columns=['PATIENT_ID', 'SAMPLE_ID']+[observation_code[key]['name'] for key in observation_code]
-    )
+    columns=['PATIENT_ID', 'SAMPLE_ID']+[observation_code[key]['name']
+                                         for key in observation_code]
+)
 
 prev_row = covid_patients_obs[0:1]
 prev_patient = prev_row['PATIENT'][0]
@@ -191,14 +217,13 @@ specimen = {
     'SAMPLE_ID': 'sample_'+str(specimen_idx)
 }
 for idx, row in covid_patients_obs.iterrows():
-    
+
     patient_id = row['PATIENT']
     day = row['days']
 
     if not ((patient_id == prev_patient) and (day == prev_day)):
         specimen_idx += 1
 
-        
         samples = samples.append(specimen, ignore_index=True)
         specimen = {
             'PATIENT_ID': patient_id,
@@ -206,7 +231,7 @@ for idx, row in covid_patients_obs.iterrows():
         }
 
     attr = observation_code[row['CODE_y']]['name']
-    if observation_code[row['CODE_y']]['type']=='':
+    if observation_code[row['CODE_y']]['type'] == '':
         observation_code[row['CODE_y']]['type'] = row['TYPE']
     value = row['VALUE']
     specimen[attr] = value
@@ -226,52 +251,60 @@ save file
 
 # remove incomplete samples
 # samples.dropna(inplace = True)
-timeline_specimen = timeline_specimen[timeline_specimen.SAMPLE_ID.isin(samples.SAMPLE_ID)]
-# 
+timeline_specimen = timeline_specimen[timeline_specimen.SAMPLE_ID.isin(
+    samples.SAMPLE_ID)]
+#
 # samples.to_csv('covid_{}_samples.txt'.format(SAMPLE_SIZE), index=False, sep='\t')
-timeline_specimen.to_csv('processedData/covid_{}_timeline_samples.txt'.format(SAMPLE_SIZE), index=False, sep='\t')
-covid_patients_obs.to_csv('processedData/covid_{}_all.txt'.format(SAMPLE_SIZE), index=False, sep='\t')
-covid_med.to_csv('processedData/covid_{}_timeline_med.txt'.format(SAMPLE_SIZE), index=False, sep='\t')
+timeline_specimen.to_csv(
+    'processedData/covid_{}_timeline_samples.txt'.format(SAMPLE_SIZE), index=False, sep='\t')
+covid_patients_obs.to_csv(
+    'processedData/covid_{}_all.txt'.format(SAMPLE_SIZE), index=False, sep='\t')
+covid_med.to_csv(
+    'processedData/covid_{}_timeline_med.txt'.format(SAMPLE_SIZE), index=False, sep='\t')
 # covert type to the format used in oncoThreads
-type_dict ={
+type_dict = {
     'numeric': 'NUMBER',
     '': 'NUMBER'
 }
 
 sample_header = [
-    "#" + '\t'.join(['Patient_Identifier', 'Sample_Identifier'] + [observation_code[key]['name'] for key in observation_code]) +'\n',
-    "#" + '\t'.join(['Patient_Identifier', 'Sample_Identifier'] + [observation_code[key]['name'] for key in observation_code]) +'\n',
-    "#" + '\t'.join(['STRING', 'STRING'] + [type_dict[observation_code[key]['type']] for key in observation_code]) +'\n',
-    "#" + '\t'.join(['1' for _ in range(2 + len(observation_code) )])+'\n',
+    "#" + '\t'.join(['Patient_Identifier', 'Sample_Identifier'] +
+                    [observation_code[key]['name'] for key in observation_code]) + '\n',
+    "#" + '\t'.join(['Patient_Identifier', 'Sample_Identifier'] +
+                    [observation_code[key]['name'] for key in observation_code]) + '\n',
+    "#" + '\t'.join(['STRING', 'STRING'] + [type_dict[observation_code[key]['type']]
+                                            for key in observation_code]) + '\n',
+    "#" + '\t'.join(['1' for _ in range(2 + len(observation_code))])+'\n',
 ]
 
 with open('processedData/covid_{}_samples.txt'.format(SAMPLE_SIZE), 'w') as txt_file:
     for line in sample_header:
         txt_file.write(line)
-    samples.to_csv(txt_file, index=False, sep='\t', na_rep= ' ')
+    samples.to_csv(txt_file, index=False, sep='\t', na_rep=' ')
 
 
-#%%
+# %%
 patient_features = ['GENDER', 'AGE', 'SURVIVE']
 
 patient_header = [
-     "#" + '\t'.join(['Patient_Identifier'] + patient_features ) +'\n',
-    "#" + '\t'.join(['Patient_Identifier'] + patient_features ) +'\n',
-    "#" + '\t'.join(['STRING', 'STRING', 'NUMBER', 'STRING']) +'\n',
-    "#" + '\t'.join(['1' for _ in range( 1+len(patient_features) )])+'\n',
+    "#" + '\t'.join(['Patient_Identifier'] + patient_features) + '\n',
+    "#" + '\t'.join(['Patient_Identifier'] + patient_features) + '\n',
+    "#" + '\t'.join(['STRING', 'STRING', 'NUMBER', 'STRING']) + '\n',
+    "#" + '\t'.join(['1' for _ in range(1+len(patient_features))])+'\n',
 ]
 
 saved_patients = sampled_inpatients[['Id', 'GENDER', 'BIRTHDATE', 'DEATHDATE']]
 saved_patients['SURVIVE'] = saved_patients['DEATHDATE'].isnull()
-saved_patients['AGE'] = np.floor((pd.to_datetime('2020-10-10') - pd.to_datetime( saved_patients['BIRTHDATE'] ))/ np.timedelta64(1, 'Y'))
+saved_patients['AGE'] = np.floor((pd.to_datetime(
+    '2020-10-10') - pd.to_datetime(saved_patients['BIRTHDATE'])) / np.timedelta64(1, 'Y'))
 saved_patients = saved_patients.rename(columns={"Id": "PATIENT_ID"})
 
 with open('processedData/covid_{}_patients.txt'.format(SAMPLE_SIZE), 'w') as txt_file:
     for line in patient_header:
         txt_file.write(line)
-    
-    saved_patients[["PATIENT_ID"]+patient_features].to_csv(txt_file, index=False, sep='\t', na_rep= ' ')
 
+    saved_patients[["PATIENT_ID"] +
+                   patient_features].to_csv(txt_file, index=False, sep='\t', na_rep=' ')
 
 
 # %%
@@ -282,13 +315,14 @@ plot
 #                     'CODE_y = 89579-7': 'High Sensitivity Cardiac Troponin I',
 #                     'CODE_y = 26881-3': 'IL-6', 'CODE_y = 731-0': 'Lymphocytes',
 #                     'CODE_y = 14804-9': 'Lactate dehydrogenase'}
-catplt = sns.catplot(x="days", y="VALUE", hue="survivor", kind="box", col='CODE_y', 
-            col_wrap=2, sharey=False, sharex=False, data=covid_patients_obs, palette=["C1", "C0"])
+catplt = sns.catplot(x="days", y="VALUE", hue="survivor", kind="box", col='CODE_y',
+                     col_wrap=2, sharey=False, sharex=False, data=covid_patients_obs, palette=["C1", "C0"])
 
 for axis in catplt.fig.axes:
     axis.xaxis.set_major_formatter(ticker.FormatStrFormatter('%d'))
     axis.xaxis.set_major_locator(ticker.MultipleLocator(base=4))
-    axis.set_title(observation_code[axis.title.get_text().replace('CODE_y = ', '')]['name'])
-        
+    axis.set_title(
+        observation_code[axis.title.get_text().replace('CODE_y = ', '')]['name'])
+
 plt.show()
 # %%
